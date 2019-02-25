b7f512319c923a88396eb0b0f32f959d345f39b4
Adam Ierymenko <adam.ierymenko@gmail.com>
2019-02-25 15:29:10 -0800

    .

--- pkg/lf/seededprng.go
+++ pkg/lf/seededprng.go
@@ -3,6 +3,7 @@ package lf
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/binary"
 
 	"golang.org/x/crypto/sha3"
 )
@@ -10,26 +11,30 @@ import (
 // seededPrng is a deterministic cryptographic random Reader used to generate key pairs from specific seeds.
 type seededPrng struct {
 	c cipher.Block
+	n uint64
 	b [16]byte
 	i int
 }
 
 func (s *seededPrng) seed(b []byte) {
 	sk := sha3.Sum384(b)
-	s.c, _ = aes.NewCipher(sk[16:48])
-	s.c.Encrypt(s.b[:], sk[0:16])
-	s.i = 0
+	s.c, _ = aes.NewCipher(sk[0:32])
+	s.n = binary.BigEndian.Uint64(sk[32:40])
+	s.i = 16
 }
 
 func (s *seededPrng) Read(b []byte) (int, error) {
+	var tmp [16]byte
 	i := s.i
 	for k := 0; k < len(b); k++ {
+		if i == 16 {
+			i = 0
+			s.n++
+			binary.BigEndian.PutUint64(tmp[0:8], s.n)
+			s.c.Encrypt(s.b[:], tmp[:])
+		}
 		b[k] = s.b[i]
 		i++
-		if i == 15 {
-			i = 0
-			s.c.Encrypt(s.b[:], s.b[:])
-		}
 	}
 	s.i = i
 	return len(b), nil
