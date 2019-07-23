2b90dc8256d42c52b92269cbc084638049a91e94
Adam Ierymenko <adam.ierymenko@gmail.com>
2019-07-23 13:59:46 -0700

    Do not require PoW or certs in local test mode.

--- pkg/lf/node.go
+++ pkg/lf/node.go
@@ -1506,6 +1506,9 @@ func (n *Node) recordApprovalStatus(rec *Record) (bool, bool) {
 	if rec == nil {
 		return false, false
 	}
+	if n.localTest {
+		return true, true
+	}
 	if !n.genesisParameters.AuthRequired && rec.ValidateWork() {
 		return true, true
 	}
@@ -1521,22 +1524,6 @@ func (n *Node) handleGenesisRecord(gr *Record) bool {
 	if err != nil {
 		n.log[LogLevelWarning].Printf("WARNING: genesis record =%s contains an invalid value, ignoring!", grHashStr)
 	} else {
-		/*
-			n.limboLock.Lock() // bogart this mutex to serialize genesis.lf writes too because no reason not to
-			genesisLF, err := os.OpenFile(path.Join(n.basePath, "genesis.lf"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-			if err != nil {
-				n.log[LogLevelWarning].Printf("WARNING: unable to write to genesis.lf: %s", err.Error())
-			}
-			if genesisLF != nil {
-				err = gr.MarshalTo(genesisLF, false)
-				if err != nil {
-					n.log[LogLevelWarning].Printf("WARNING: unable to write to genesis.lf: %s", err.Error())
-				}
-				genesisLF.Close()
-			}
-			n.limboLock.Unlock()
-		*/
-
 		if len(rv) > 0 && atomic.LoadUint64(&n.lastGenesisRecordTimestamp) < gr.Timestamp {
 			n.log[LogLevelNormal].Printf("applying genesis configuration update from record =%s", grHashStr)
 			n.genesisParameters.Update(rv)
